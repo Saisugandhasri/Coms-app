0b4ef4d28fb7045cdc8e00f9a9af2f9eb48bd64f
IAMVENEETH <whoisveneeth@gmail.com>
2026-01-01 11:15:50 +0530

    testing-4

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,43 +1,4 @@
-# from fastapi import APIRouter
-# from app.models.schemas import ContentRequest, ContentResponse
-# from app.services.content_generator import generate_content
-# from app.services.assessment_service import (
-#     start_assessment,
-#     save_answer,
-#     submit_assessment
-# )
-# from app.models.schemas import (
-#     StartAssessmentResponse,
-#     SaveAnswerRequest,
-#     SubmitAssessmentRequest,
-#     SubmitAssessmentResponse
-# )
-
-# router = APIRouter(prefix="/generate", tags=["Content Generation"])
-# router_1 = APIRouter(prefix="/assessment", tags=["Assessment"])
-
-# @router.post("/", response_model=ContentResponse)
-# async def generate(request: ContentRequest):
-#     return generate_content(request.topic)
-
-# @router_1.post("/start", response_model=StartAssessmentResponse)
-# async def start(topic: ContentRequest):
-#     return start_assessment(topic.topic)
-
-# @router_1.post("/answer")
-# async def answer(request: SaveAnswerRequest):
-#     return save_answer(
-#         request.assessment_id,
-#         request.question_id,
-#         request.selected_answer
-#     )
-# @router_1.post("/submit", response_model=SubmitAssessmentResponse)
-# async def submit(request: SubmitAssessmentRequest):
-#     return submit_assessment(
-#         request.assessment_id,
-#     )
-
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, UploadFile, File
 from app.models.schemas import (
     StartAssessmentRequest,
     StartAssessmentResponse,
@@ -48,6 +9,10 @@ from app.services.assessment_service import (
     start_assessment,
     submit_assessment,
 )
+
+
+
+
 
 router = APIRouter(prefix="/assessment", tags=["Assessment"])
 
--- backend/app/core/llm_client.py
+++ backend/app/core/llm_client.py
@@ -1,26 +1,4 @@
-# import requests
-# # from app.core.config import LLM_MODEL
-
-# OLLAMA_URL = "http://localhost:11434/api/generate"
-
-# def call_llm(prompt: str) -> str:
-#     payload = {
-#         "model": "nuextract",
-#         "prompt": prompt,
-#         "stream": False,
-#         "format": "json"
-#     }
-#     try:
-#         response = requests.post(OLLAMA_URL, json=payload,timeout=60)
-#         response.raise_for_status()
-#         return response.json()["response"]
-#     except requests.exceptions.RequestException as e:
-#         raise RuntimeError(
-#             f"LLM service unavailable: {str(e)}"
-#         )
-
 import requests
-
 
 def call_llm(prompt: str) -> str:
     try:
